07fbd09e74097780f3a6f447a9902d8eb099e511
Giannandrea <34889642+Giannandrea-Vicidomini@users.noreply.github.com>
2020-09-01 21:55:29 +0200

    cm

--- rngWalker/sketch.js
+++ rngWalker/sketch.js
@@ -1,11 +1,38 @@
 
+
+let time
+let trail1
+let walker
+
+let operator = (x,y,color)=>{
+	angleMode(DEGREES)
+	let radius = 50
+	let x1 = radius* cos(time)
+	let y1 = radius* sin(time)
+
+	stroke(255)
+	fill(color)
+	ellipse(x+x1,y+y1,5,5)
+	//point(x+x1,y+y1)
+
+}
+
 function setup() {
+
 	createCanvas(600, 600);
+	trail1 = new TrailRenderer(color(0,0,0),operator)
+	walker = new Walker(width/2,height/2,trail1)
+	time = 0
+	console.log(walker);
 	
 }
 
 function draw() {
-
+	
+	time+=1.5
+	walker.walk(random(-4,4),random(-4,4))
+	//walker.walk(mouseX,mouseY)
+	walker.show()
 }
 
 class Walker{
@@ -18,10 +45,23 @@ class Walker{
 	walk(xOffset,yOffset){
 		this.x+=xOffset
 		this.y+=yOffset
+		/* let r = random(0,4)
+		if(r==0){
+			this.x+=1
+		}
+		else if(r==1){
+			this.x-=1
+		}
+		else if (r==2){
+			this.y+=1
+		}
+		else{
+			this.y-=1
+		} */
 	}
 	
 	show(){
-		trail.show(this.x,this.y)
+		this.trail.show(this.x,this.y)
 	}
 
 }
@@ -33,6 +73,6 @@ class TrailRenderer{
 	}
 
 	show(x, y){
-		this.behaviour(x,y)
+		this.behaviour(x,y,this.color)
 	}
 }
