be6ff6ca22eeac50e258fd843fc1f246bfa3d681
Giannandrea <34889642+Giannandrea-Vicidomini@users.noreply.github.com>
2020-09-01 20:53:24 +0200

    second commit

--- rngWalker/sketch.js
+++ rngWalker/sketch.js
@@ -1,7 +1,30 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(600, 600);
 }
 
 function draw() {
 
+}
+
+class Walker{
+	constructor(x,y,trail){
+		this.x = x 
+		this.y = y
+	}
+
+	walk(xOffset,yOffset){
+		this.x+=xOffset
+		this.y+=yOffset
+	}
+	
+	show(){
+
+	}
+
+}
+
+class TrailRenderer{
+	constructor(color){
+		
+	}
 }
